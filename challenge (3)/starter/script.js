c887c99f66a4596f7611338df385983d46e27f22
Ryan Taylor <ryantaylor50@hotmail.com>
2023-01-06 22:44:38 +0000

    updated functions, prompts not showing

--- challenge (3)/starter/script.js
+++ challenge (3)/starter/script.js
@@ -88,9 +88,33 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+//alert("Hey there, lets get you a password");
 // Function to prompt user for password options
 function getPasswordOptions() {
+  
+  var choiceArr = [];
+  let passLength = parseInt(prompt("Enter a password length between 10 and 64"));
+    if(isNaN(passLength) || passLength < 10 || passLength > 64) {
+      alert("Password has to be between 10 and 64 characters, please try again");
+    return false;
+    }
+    if (confirm("Would you like to use lowercase letters?")){
+    choiceArr = choiceArr.concat(lowerCasedCharacters);
 
+    }
+    if (confirm("Would you like to use uppercase letters?")){
+    choiceArr = choiceArr.concat(upperCasedCharacters);
+
+    }
+    if (confirm("Would you like to use numbers letters?")){
+    choiceArr = choiceArr.concat(numericCharacters);
+
+    }
+    if (confirm("Would you like to use special characters?")){
+    choiceArr = choiceArr.concat(specialCharacters);
+
+    }
+  return true;
 }
 
 // Function for getting a random element from an array
@@ -100,6 +124,12 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
+  var password = "";
+  for(var i = 0; i , passLength; i++) {
+      var randomIndex = Math.floor(Math.random() * choiceArr.length);
+      password = password + choiceArr[randomIndex];
+  }
+  return password;
 
 }
 
@@ -108,11 +138,43 @@ var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
+  var correctPrompts = getPrompts();
+    if (correctPrompts) {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
+    }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+// My working
+//alert("Hi There! Lets make you a password")
+function getPrompts() {
+  
+}
+
+
+// My additions
+ //var length = Number(prompt("Enter a password length between 8 and 128")),
+//   charType = prompt("Enter a character type: special, numeric, uppercase, lowercase."),
+//   password = generatePassword();
+// document.getElementById("display").value = password;
+// document.getElementById('copy-btn').addEventListener('click', copyPassword);
+
+// function generatePassword() {
+//   var charSets = {
+//     lowercase: 'abcdefghijklmnopqrstuvwxyz',
+//     uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+//     numeric: '0123456789',
+//     special: ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+//   };
+//   var charSet = charSets[charType.toLowerCase()] || charSets.lowercase;
+//   var retVal = "";
+//   for (var i = 0; i < length; i++) {
+//     retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+//   }
+//   return retVal;
+// }
